9b8aa7fa290f741ac93d1c1777a941ace0412771
George <user@users-MacBook-Pro.local>
2026-08-14 22:31:09 +0400

    fixed type errors

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import time
 app = Flask(__name__)
 
 @app.route("/")
-def get_current_time();
+def get_current_time():
     current_time = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
     return f"The current time of the day: {current_time}"
 
